cfb50340d0e05296bae02fff4c8b603f0be9db93
Pavan kalyan Vallem <116752882+vallempavan2002@users.noreply.github.com>
2023-03-01 15:08:12 +0530

    java..ternory..operatoer(conditional operatoer)

---
+++ ternory.java
@@ -0,0 +1,10 @@
+//ternary operator;;;;
+   public class Main 
+   {
+       public static void Main(String[]args)
+       {
+    int year =1999;
+    String result =(year%4==0)?"leap year":"not leap year";
+    System.out.println(result);
+   }
+   }
